a5b85ef66ed45a57ef4c11686fbf5f0a649ff54f
harrisonchuckl <harrison@chuckl.co.uk>
2025-09-13 13:13:00 +0100

    Update logging_utils.py

--- src/logging_utils.py
+++ src/logging_utils.py
@@ -1,1 +1,13 @@
+import logging
+import sys
+
+def get_logger(name="contact_finder"):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        fmt = logging.Formatter("[%(levelname)s] %(message)s")
+        handler.setFormatter(fmt)
+        logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
 
